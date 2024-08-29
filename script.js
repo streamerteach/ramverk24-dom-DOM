e600e2ed6802294249688582a66d61ba02880062
gerkja <gerkmanj@arcada.fi>
2024-08-29 11:55:25 +0300

    testing eventhandlers

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 window.onload = function() {
     // Lyssna efter händelser
+    let teamMember = document.getElementsByClassName("team-member")
+
+    let mouseover = function(event) {
+        this.style.border = "3px solid";
+    }
+
+    let mouseoff = function(event) {
+        this.style.border = "none";
+    }
+
+    for (let i = 0; i < teamMember.length; i++) {
+        teamMember[i].addEventListener("mouseover", mouseover)
+        teamMember[i].addEventListener("mouseoff", mouseoff)
+    }
 }
 var editTitle = function(e) {
     // Ändra "team-title"
 };
 
-function effectOn(id){
+function effectOn(id) {
     // Rita en ram runt personen, kanske lite drop shadow?
+    
+    teamMember.onmouseover 
+        this.style.border
+    
 }
 
 function effectOff(id){
