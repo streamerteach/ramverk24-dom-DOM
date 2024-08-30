ac556230b67317afdb142275e979d3bbee63c9aa
Max Roos <roosmaxm@arcada.fi>
2024-08-30 12:01:37 +0300

    mouseout och mouseover done

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 window.onload = function() {
     // Lyssna efter händelser
+    effectOn();
+    effectOff();
 }
 var editTitle = function(e) {
     // Ändra "team-title"
 };
-
 function effectOn(id){
     // Rita en ram runt personen, kanske lite drop shadow?
+    const images = document.querySelectorAll('.team-member');
+
+    images.forEach(function (image) {
+        image.addEventListener('mouseover', function() {
+            image.style.border = '5px solid black';
+        });
+    });
 }
+
 
 function effectOff(id){
     // Stäng av effekten när musen inte längre är ovanför personen
+    const images = document.querySelectorAll('.team-member');
+
+    images.forEach(function (image) {
+        image.addEventListener('mouseout', function() {
+            image.style.border = '';
+         });
+    });
 }
